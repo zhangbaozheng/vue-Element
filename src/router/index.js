09f92b5006f13ca8e5a2973941514374de6526ac
zhangbaozheng <488937163@qq.com>
2020-12-27 20:35:53 +0800

    宝宝分支1

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: '/fater',
+    name: 'Fater',
+    component: () => import('../views/Fater.vue')
+  }
 ];
 
 const router = new VueRouter({
---
+++ src/views/Children.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-03 11:55:54
+ * @LastEditTime: 2020-12-03 11:56:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Editre
+ * @FilePath: \my-app\src\views\Children.vue
+-->
+<template>
+  <div>
+    <span v-for="(item, index) in list" :key="index">
+      {{ item.name }} -- {{ item.age }}
+      <button @click="EditAge(index)">点击修改名字</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array,
+  },
+  mounted() {
+    console.log(this.list, "父组件的值");
+  },
+  methods: {
+    EditAge(index) {
+      this.$emit("editage", index);
+      console.log(index);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
